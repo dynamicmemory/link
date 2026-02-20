01a7abba60f7f048647045abc63298920a1373b5
dynamicmemory <joshua.hahn@live.com>
2026-02-20 15:48:37 +1000

    Basic structure for server multiplexing in

--- link.cpp
+++ link.cpp
@@ -42,16 +42,6 @@ public:
         rhs.fd = -1;
     }
 
-    TCPSocket &operator=(TCPSocket &&rhs) {
-        if (this == &rhs) return *this;
-        if (fd > 0) {
-            ::close(fd);
-            fd = rhs.fd;
-            rhs.fd = -1;
-        }
-        return *this;
-    }
-
     static TCPSocket server_socket(const std::string &host, const std::string &port) {
         struct addrinfo *addr = address(host, port, true); 
         int fd = make_socket(addr);
@@ -111,8 +101,6 @@ public:
 
     ssize_t send_all() { /*Write send_all logic*/ return 0; }
     ssize_t recieve_all() { /*Write send_all logic*/ return 0; }
-
-    int get_fd() { return fd; }
 };
 
 
@@ -129,60 +117,10 @@ class Server {
 public:
     Server(const std::string &h, const std::string &p, const std::string &protocol) : 
         socket(TCPSocket::server_socket(h, p)) {
-        }
-
-    void start_server() {
         socket.listen();
+
         socket.accept_client();
-
-        fd_set master;
-        int max_fd = socket.get_fd();
-        
-        struct timeval timeout;
-        timeout.tv_sec = 0;
-        timeout.tv_usec = 100000;
-
-        while (1) {
-            fd_set fds = master;
-            if (select(max_fd+1, &fds, 0, 0, &timeout) < 0) {
-                throw std::runtime_error("select failed");
-            }
-
-            for (int fd=0; fd<=max_fd; ++fd) {
-                if (!FD_ISSET(fd, &fds)) continue;
-
-                // Client connecting to the server
-                if (fd == socket.get_fd()) {
-                    TCPSocket new_client = socket.accept_client();
-                    // failed to connect, might not need check due to throw.
-                    if (new_client.get_fd() < 0) continue; 
-                    clients.push_back(std::move(new_client));
-                    FD_SET(new_client.get_fd(), &master);
-                    if (new_client.get_fd() > max_fd) max_fd = new_client.get_fd();
-
-                }
-                else {
-                    ssize_t total = 1024;
-                    char buf[total];
-                    ssize_t n = ::recv(fd,buf,total,0);
-
-                    // Client disconnecting 
-                    if (n == 0) {
-                        // Consider using find instead of the loop
-                        for (auto it=clients.begin(); it != clients.end(); ++it)
-                            if (it->get_fd() == fd) it = clients.erase(it);
-
-                        FD_CLR(fd, &master);
-                        if (fd == max_fd)
-                            while (max_fd >= 0 && !FD_ISSET(max_fd, &fds))
-                                max_fd--;
-                    }
-                    // Client sending a request
-                    continue;
-                }
-            }
         }
-    }
 };
 
 
@@ -228,32 +166,12 @@ public:
         Client(host, port);
     }
 
-    void start_server() {
-        // Hard coded only first server for now.
-        servers[0]->start_server();
-    }
-
     void send() {}
     void recieve() {}
 
     void disconnect() {}
     void shutdown() {}
 };
-
-
-// Main for testing 
-int main(int argc, char **argv) {
-    Network n;
-    std::string host = "127.0.0.1";
-    std::string port = "4444";
-
-    n.create_server(host, port, "protocol");
-    n.start_server();
-    // n.create_client(host, port);
-
-    return 0;
-}
-
 
 class Connection {
 public: 
